804a2873297b4be96fe55cf04d45625cfadeb304
JMarianaStark <marianareyes9@gmail.com>
2021-02-24 12:26:07 -0600

    Hola mundo commit

--- src/mundo.java
+++ src/mundo.java
@@ -1,7 +1,6 @@
 public class mundo {
-    public static void main(String[] args){
-        System.out.println(" Hola mundo ");
+    public static void main(String[] args ){
+        System.out.println("Hola mundo ");
         System.out.println("Adios mundo");
     }
-
 }
